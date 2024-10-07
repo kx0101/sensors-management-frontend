419dc8c2bc148e46b733524c58b25df6ae4a8b89
kx0101 <liakos.koulaxis@yahoo.com>
2024-10-07 08:29:56 +0300

    feat: vite build

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,16 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [vue()],
-})
+    build: {
+        rollupOptions: {
+            output: {
+                entryFileNames: 'ui.js',
+                chunkFileNames: 'ui.js',
+                assetFileNames: 'ui.[ext]',
+                manualChunks: undefined,
+            }
+        }
+    }
+});
